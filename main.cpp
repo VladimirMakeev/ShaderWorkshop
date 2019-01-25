18b5c398ca5173f868177a331e422d1c382e752e
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2019-01-26 01:23:44 +0300

    Setup OpenGL 3.3 core profile context before application starts

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include "shaderworkshop.h"
 #include <QApplication>
+#include <QSurfaceFormat>
 
 int main(int argc, char *argv[])
 {
+    // ShaderWorkshop expects OpenGL 3.3 core profile context
+    QSurfaceFormat format;
+    format.setDepthBufferSize(24);
+    format.setStencilBufferSize(8);
+    format.setVersion(3, 3);
+    format.setProfile(QSurfaceFormat::CoreProfile);
+
+    QSurfaceFormat::setDefaultFormat(format);
+
     QApplication a(argc, argv);
     ShaderWorkshop w;
     w.show();
