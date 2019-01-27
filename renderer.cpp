0015cb7d5b757f5f28c6c78609d827af7dce3fab
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2019-01-28 02:25:15 +0300

    Add main image effect to renderer

--- renderer.cpp
+++ renderer.cpp
@@ -1,13 +1,22 @@
 #include "renderer.h"
 
-Renderer::Renderer(QWidget *parent) : QOpenGLWidget(parent)
+Renderer::Renderer(QWidget *parent) :
+    QOpenGLWidget(parent),
+    fboTextureSize(1024, 768)
 {
-
 }
 
 Renderer::~Renderer()
 {
+    makeCurrent();
 
+    vbo.release();
+    vao.release();
+
+    delete vertexShader;
+    delete mainImage;
+
+    doneCurrent();
 }
 
 void Renderer::initializeGL()
@@ -15,14 +24,153 @@ void Renderer::initializeGL()
     initializeOpenGLFunctions();
 
     glClearColor(1.0f, 0.0f, 0.4f, 1.0f);
+
+    setupVertexShader();
+
+    setupBuffers();
+
+    mainImage = createEffect();
 }
 
 void Renderer::resizeGL(int w, int h)
 {
+    viewSize = QSize(w, h);
     QOpenGLWidget::resizeGL(w, h);
 }
 
 void Renderer::paintGL()
 {
-    QOpenGLWidget::paintGL();
+    Q_ASSERT(mainImage != Q_NULLPTR);
+
+    bool result = QOpenGLFramebufferObject::bindDefault();
+
+    Q_ASSERT(result == true);
+
+    glViewport(0, 0, viewSize.width(), viewSize.height());
+
+    renderEffect(*mainImage, viewSize);
+    mainImage->frame++;
+}
+
+QString Renderer::defaultFragmentShader() const
+{
+    return QString{
+        "#version 330 core\n"
+        "\n"
+        "out vec4 fragColor;\n"
+        "\n"
+        "void main(void) {\n"
+        "fragColor = vec4(0.0, 1.0, 0.5, 1.0);\n"
+        "}\n"
+    };
+}
+
+void Renderer::setupVertexShader()
+{
+    const QString vertexSource{
+        "#version 330 core\n"
+        "layout (location = 0) in vec2 pos;\n"
+        "void main() {\n"
+        "gl_Position = vec4(pos, 0.0, 1.0);\n"
+        "}\n"
+    };
+
+    vertexShader = new QOpenGLShader(QOpenGLShader::ShaderTypeBit::Vertex, this);
+
+    bool result = vertexShader->compileSourceCode(vertexSource);
+
+    Q_ASSERT(result == true);
+}
+
+void Renderer::setupBuffers()
+{
+    bool result = vao.create();
+
+    Q_ASSERT(result == true);
+
+    vao.bind();
+
+    result = vbo.create();
+
+    Q_ASSERT(result == true);
+
+    result = vbo.bind();
+
+    Q_ASSERT(result == true);
+
+    const GLfloat vertices[] = {
+        -1.0, 1.0,
+        -1.0, -1.0,
+        1.0, -1.0,
+
+        -1.0, 1.0,
+        1.0, -1.0,
+        1.0, 1.0
+    };
+
+    vbo.setUsagePattern(QOpenGLBuffer::UsagePattern::StaticDraw);
+    vbo.allocate(vertices, sizeof(vertices));
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), Q_NULLPTR);
+}
+
+Effect* Renderer::createEffect()
+{
+    QOpenGLShader *fragment = new QOpenGLShader(QOpenGLShader::ShaderTypeBit::Fragment, this);
+    QString source = defaultFragmentShader();
+
+    bool result = fragment->compileSourceCode(source);
+
+    Q_ASSERT(result == true);
+
+    QOpenGLShaderProgram *program = new QOpenGLShaderProgram(this);
+
+    result = program->addShader(vertexShader);
+
+    Q_ASSERT(result == true);
+
+    result = program->addShader(fragment);
+
+    Q_ASSERT(result == true);
+
+    result = program->link();
+
+    Q_ASSERT(result == true);
+
+    QOpenGLFramebufferObject *fbo = new QOpenGLFramebufferObject(fboTextureSize);
+
+    return new Effect(program, fragment, fbo, source);
+}
+
+void Renderer::renderEffect(Effect &effect, QSize textureSize)
+{
+    Q_UNUSED(textureSize);
+
+    bindEffectTextures(effect);
+
+    auto program = effect.program;
+
+    Q_ASSERT(program != Q_NULLPTR);
+
+    bool result = program->bind();
+
+    Q_ASSERT(result == true);
+
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+}
+
+void Renderer::bindEffectTextures(const Effect &effect)
+{
+    int textureUnit = 0;
+
+    for (auto &input : effect.inputs) {
+        glActiveTexture(GL_TEXTURE0 + textureUnit);
+
+        auto otherEffect = input.effect;
+        // if there is no input effect used, unbind texture
+        GLuint id = otherEffect ? otherEffect->framebuffer->texture() : 0;
+
+        glBindTexture(GL_TEXTURE_2D, id);
+    }
 }
--- renderer.h
+++ renderer.h
@@ -3,6 +3,10 @@
 
 #include <QOpenGLWidget>
 #include <QOpenGLFunctions>
+#include <QOpenGLShader>
+#include <QOpenGLVertexArrayObject>
+#include <QOpenGLBuffer>
+#include "effect.h"
 
 class Renderer : public QOpenGLWidget, protected QOpenGLFunctions
 {
@@ -16,6 +20,27 @@ protected:
     void initializeGL() Q_DECL_OVERRIDE;
     void resizeGL(int w, int h) Q_DECL_OVERRIDE;
     void paintGL() Q_DECL_OVERRIDE;
+
+private:
+    QString defaultFragmentShader() const;
+    void setupVertexShader();
+    void setupBuffers();
+
+    Effect* createEffect();
+    void renderEffect(Effect &effect, QSize textureSize);
+    /// bind textures according to this effect input channels settings,
+    /// set sampler settings
+    void bindEffectTextures(const Effect &effect);
+
+    Effect *mainImage;
+    /// vertex shader used for all effects
+    QOpenGLShader *vertexShader;
+
+    QOpenGLVertexArrayObject vao;
+    QOpenGLBuffer vbo;
+
+    QSize fboTextureSize;
+    QSize viewSize;
 };
 
 #endif // RENDERER_H
