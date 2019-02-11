ebf4e0cbe10907e622b8fdf05aa31725be2e0caa
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2019-02-11 23:40:42 +0300

    Add license notices

--- renderer.h
+++ renderer.h
@@ -1,3 +1,21 @@
+/*
+ * This file is part of ShaderWorkshop (https://github.com/VladimirMakeev/ShaderWorkshop).
+ * Copyright (C) 2019 Vladimir Makeev.
+ *
+ * ShaderWorkshop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * ShaderWorkshop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ShaderWorkshop.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef RENDERER_H
 #define RENDERER_H
 
